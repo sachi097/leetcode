681d4c5e6428cfea066bcef77643be978479aecf
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2023-09-13 12:54:48 -0600

    0334-increasing-triplet-subsequence.cpp

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -3,6 +3,9 @@ public:
     bool increasingTriplet(vector<int>& nums) {
         int n=nums.size();
         if(n<3)return false;       //if size of array is less than 3 we can't make triplate
+        //find low, mid such that it is less than large.
+        //current number is low if it is < current low
+        //current number is mid if it is in between current low and current mid
         int low=INT_MAX, mid=INT_MAX;
         for(int i=0;i<n;i++)
         {
