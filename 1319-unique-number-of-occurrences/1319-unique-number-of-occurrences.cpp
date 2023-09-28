34d4958e414515b6c0fd0bced689c5c56cd7e3c0
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2023-09-28 02:09:05 -0600

    Time: 2 ms (62.64%), Space: 8.7 MB (10.53%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> hash;
+        unordered_set<int> set;
+        for(auto i = 0; i < arr.size(); i++){
+            hash[arr[i]] = 0;
+        }
+        for(auto i = 0; i < arr.size(); i++){
+            hash[arr[i]]++;
+        }
+        for(auto el: hash){
+            if(set.find(el.second) != set.end())
+                return false;
+            set.insert(el.second);
+        }
+        return true;
+    }
+};
