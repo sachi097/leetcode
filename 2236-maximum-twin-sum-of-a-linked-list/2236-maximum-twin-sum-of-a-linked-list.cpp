d0eee3afccdda6e29d57f11aec789e571cf10aff
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2023-10-09 02:30:42 -0600

    Time: 146 ms (98.94%), Space: 119.5 MB (51.32%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -13,18 +13,18 @@ public:
     int pairSum(ListNode* head) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        unordered_map<int,int> hashMap;
+        stack<int> st;
         ListNode* slow = head;
         ListNode* fast = head;
         int count = 0, maximum = INT_MIN;
         while(fast && fast->next){
-            hashMap[count++] = slow->val;
+            st.push(slow->val);
             slow = slow->next;
             fast = fast->next->next;
         }
-        count--;
         while(slow){
-            maximum = max(maximum, hashMap[count--] + slow->val);
+            maximum = max(maximum, st.top() + slow->val);
+            st.pop();
             slow = slow->next;
         }
         return maximum;
