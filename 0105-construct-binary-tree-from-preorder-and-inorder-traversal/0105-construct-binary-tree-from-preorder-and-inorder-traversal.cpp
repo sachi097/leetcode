442a68124b77596cf2e5804e7075771fc64f140b
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2023-10-11 00:20:14 -0600

    Time: 11 ms (81.51%), Space: 26 MB (86.6%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,15 +11,33 @@
  */
 class Solution {
 public:
-    int rootI = 0;
+    // this is needed outside to keep rootIndex within size of preorder 
+    int rootIndex = 0;
+    
     TreeNode* buildBT(vector<int>& preorder, vector<int>& inorder, int start, int end){
+        // boundary condition when end < start i.e array out of bound
         if(end < start) return NULL;
-        TreeNode* root = new TreeNode(preorder[rootI++]);
+
+        // pick root from preorder
+        TreeNode* root = new TreeNode(preorder[rootIndex]);
+
+        // increment rootIndex because next element in preorder will be the root for subtree
+        rootIndex++;
+
+        // find current root position in inorder and create left subtree and right subtree.
+        // left subtree will have all the elements on the left of current root in inorder
+        // right subtree will have all the elements on the right of current root in inorder
         auto rootIteratorInOrder = find(inorder.begin() + start, inorder.begin() + end, root->val);
         if(rootIteratorInOrder != inorder.end()){
-                int newStart = (rootIteratorInOrder - inorder.begin()) + 1, newEnd = (rootIteratorInOrder - inorder.begin()) - 1;
-                root->left = buildBT(preorder,inorder, start, newEnd);
-                root->right = buildBT(preorder,inorder, newStart, end);        
+            // left subtree end
+            int newEnd = (rootIteratorInOrder - inorder.begin()) - 1;
+
+            // right subtree start
+            int newStart = (rootIteratorInOrder - inorder.begin()) + 1;
+
+            //build left and right subtree of current root with newEnd and newStart
+            root->left = buildBT(preorder,inorder, start, newEnd);
+            root->right = buildBT(preorder,inorder, newStart, end);        
         }
         return root;
     }
