1762ea5909478e515a4d3e61b5de1eb5ad0a34ad
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2023-11-25 01:42:24 -0700

    Time: 362 ms (46.29%), Space: 90 MB (84.26%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,20 +3,37 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size(), remove = 0;
         sort(intervals.begin(), intervals.end());
-        stack<pair<int,int>> s;
-        for(auto i=0; i<n; i++){
+        
+        // using stack
+        // stack<pair<int,int>> s;
+        // for(auto i=0; i<n; i++){
+        //     pair<int,int> curInt = {intervals[i][0], intervals[i][1]};
+        //     if(!s.empty() && curInt.first < s.top().second){
+        //         if(s.top().second > curInt.second){
+        //             s.pop();
+        //             s.push(curInt);
+        //         }
+        //         remove++;
+        //     }
+        //     else{
+        //         s.push(curInt);
+        //     }
+        // }
+        
+        pair<int,int> lastInt = {intervals[0][0], intervals[0][1]};
+        for(auto i=1; i<n; i++){
             pair<int,int> curInt = {intervals[i][0], intervals[i][1]};
-            if(!s.empty() && curInt.first < s.top().second){
-                if(s.top().second > curInt.second){
-                    s.pop();
-                    s.push(curInt);
+            if(curInt.first < lastInt.second){
+                if(lastInt.second > curInt.second){
+                    lastInt = curInt;
                 }
                 remove++;
             }
             else{
-                s.push(curInt);
+                lastInt = curInt;
             }
         }
+        
         return remove;
     }
 };
