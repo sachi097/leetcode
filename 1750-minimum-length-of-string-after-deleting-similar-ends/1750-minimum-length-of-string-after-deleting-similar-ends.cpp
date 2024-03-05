ad21cf498d360f1c9bf0669ca7216f03890fbb85
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2024-03-04 22:54:57 -0700

    Time: 31 ms (18.28%), Space: 14 MB (13.45%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n = s.size();
-        int left = 0, right = n - 1;
+        int left = 0, right = n - 1, preStartIndex, postStartIndex;
         while(left < right){
+            preStartIndex = left;
+            postStartIndex = right;
             while(left < n - 1 && s[left] == s[left+1])
                 left++;
             while(right > 0 && s[right] == s[right-1])
                 right--;
             if(s[left] != s[right]){
-                while(left > 0 && s[left] == s[left-1])
-                    left--;   
-                while(right < n - 1 && s[right] == s[right+1])
-                    right++;
-                return right - left + 1;
+                return postStartIndex - preStartIndex + 1;
             }
             else{
                 left++;
                 right--;
             }
         }
-        dfjkhfjkhgfghfj
         return right - left == 0 ? 1 : 0;
     }
 };
