1f17132e8262e457a7ab405b4b2eeb0819866efb
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2024-03-12 22:46:50 -0600

    Time: 3 ms (51.39%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -4,13 +4,11 @@ public:
         return (n * (n+1)) / 2;
     }
     int pivotInteger(int n) {
-        int low = 1, high = n, mid, leftSum, rightSum;
+        int low = 1, high = n, mid, leftSum, rightSum, nSum = getSum(n);
         while(low <= high){
             mid = low + (high - low) / 2;
-            cout<<mid<<endl;
             leftSum = getSum(mid);
-            rightSum = getSum(n) - leftSum + mid;
-            cout<<low<<" "<<leftSum<<" "<<high<<" "<<rightSum<<endl;
+            rightSum = nSum - leftSum + mid;
             if(leftSum == rightSum){
                 return mid;
             }
