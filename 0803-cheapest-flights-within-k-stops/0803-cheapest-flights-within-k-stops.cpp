b935baaa09874154bab9dc029acb8aebb122d20f
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2024-10-27 18:38:31 -0600

    Time: 0 ms (100%), Space: 17.2 MB (67.94%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    using int2 = pair<int, int>;
+    using int3 = tuple<int, int, int>;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
+    {
+        vector<int> distance(n, INT_MAX);
+        
+        unordered_map<int, vector<pair<int, int>>> adj;
+        
+        for(vector<int> &vec : flights) {
+            int u    = vec[0];
+            int v    = vec[1];
+            int cost = vec[2];
+            
+            adj[u].push_back({v, cost});
+        }
+        
+        queue<pair<int, int>> que;
+        que.push({src, 0});
+        distance[src] = 0;
+        
+        int level = 0;
+        
+        while(!que.empty() && level <= k) {
+            int N = que.size();
+            
+            while(N--) {
+                int u = que.front().first;
+                int d = que.front().second;
+                que.pop();
+                
+                for(pair<int, int> &P : adj[u]) {
+                    
+                    int v    = P.first;
+                    int cost = P.second;
+                    
+                    if(distance[v] > d + cost) {
+                        distance[v] = d + cost;
+                        que.push({v, d+cost});
+                    }
+                    
+                }
+                
+            }
+            level++;
+        }
+        
+        return distance[dst] == INT_MAX ? -1 : distance[dst];
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
