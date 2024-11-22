82fb5840b072f58ed89c7efcb974232924c086fb
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2024-11-22 18:38:58 +0530

    Time: 135 ms (62.05%), Space: 9.4 MB (82.65%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    int m, n;
+
+    int dfs(int i, int j, vector<vector<int>>& grid){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] == 0){
+            return 0;
+        }
+
+        int result = 0;
+        int tmp = grid[i][j];
+        grid[i][j] = 0; // do
+
+        for(int k=0; k<4; k++){ // explore
+            int nextI = i + dir[k][0];
+            int nextJ = j + dir[k][1];
+            result = max(result, dfs(nextI, nextJ, grid));
+        }
+
+        grid[i][j] = tmp; // undo
+        return grid[i][j] + result;
+    }
+    
     int getMaximumGold(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        m = grid.size();
+        n = grid[0].size();
         int maxGold = 0;
 
-        // Helper function for DFS
-        function<int(int, int)> helper = [&](int row, int col) -> int {
-            if (row < 0 || row >= m || col < 0 || col >= n ||
-                grid[row][col] == 0) {
-                return 0;
-            }
-
-            int currentGold = grid[row][col];
-            grid[row][col] = 0; // Mark the cell as visited by setting it to 0
-
-            // Recursively explore all four directions
-            int d = helper(row + 1, col);
-            int u = helper(row - 1, col);
-            int r = helper(row, col + 1);
-            int l = helper(row, col - 1);
-
-            // Restore the cell value
-            grid[row][col] = currentGold;
-
-            return currentGold + max({d, u, r, l});
-        };
-
-        // Iterate over all cells in the grid
-        for (int row = 0; row < m; ++row) {
-            for (int col = 0; col < n; ++col) {
-                if (grid[row][col] > 0) {
-                    maxGold = max(maxGold, helper(row, col));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    maxGold = max(maxGold, dfs(i, j, grid));
                 }
             }
         }
