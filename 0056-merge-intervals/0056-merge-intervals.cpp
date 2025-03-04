e01672161083c427af731feb187005a30d8582de
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-03-04 00:00:58 -0700

    Time: 7 ms (44.25%), Space: 24.9 MB (15.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,44 +1,23 @@
 class Solution {
+private:
+    void fast(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [&](auto a, auto b){
-            return a[0] < b[0];
-        });
-
-        int lastStart, lastEnd, currentStart, currentEnd;
-
-        vector<vector<int>> ans;
-        ans.push_back({intervals[0][0], intervals[0][1]});
-        
-        for(int i=1; i<intervals.size(); i++){
-            auto lastInterval = ans.back();
-            lastStart = lastInterval[0];
-            lastEnd = lastInterval[1];
-
-            currentStart = intervals[i][0];
-            currentEnd = intervals[i][1];
-
-            if(lastStart <= currentStart && currentEnd <= lastEnd){
-                continue;
-            }
-
-            if(lastEnd < currentStart){
-                ans.push_back({currentStart, currentEnd});
-                continue;
-            }
-
-            if(lastStart < currentEnd && currentEnd < lastEnd){
-                ans.back()[0] = currentStart;
-            }
-            else if(lastStart < currentStart & currentStart <= lastEnd){
-                ans.back()[1] = currentEnd;
+        fast();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        for(auto num: intervals){
+            if(res.empty() || res.back()[1] < num[0]){
+                res.push_back(num);
             }
             else{
-                ans.back()[0] = currentStart;
-                ans.back()[1] = currentEnd;
+                res.back()[1]=max(res.back()[1], num[1]);
             }
-
         }
-        return ans;
+        return res;
     }
 };
