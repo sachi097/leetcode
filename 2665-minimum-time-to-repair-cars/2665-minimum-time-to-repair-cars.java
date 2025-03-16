5f576d03029b927d68e5dba9553cca82780d5169
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-03-15 19:54:26 -0600

    Time: 88 ms (19.09%), Space: 55.2 MB (85.75%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean find(long mid, int[] ranks, int cars){
+        long numberCars = 0;
+        for(int i=0; i<ranks.length; i++){
+            numberCars += Math.floor(Math.sqrt(mid / ranks[i]));
+            if(numberCars >= cars){
+               return true;
+            }
+        }
+        return false ;
+    }
+
+
+    public long repairCars(int[] ranks, int cars) {
+        long l = 1, h = Long.MAX_VALUE;
+        long ans = h;
+        while(l<=h){
+            long mid = (l + h) / 2;
+            if(find(mid, ranks, cars)){
+                ans = mid;
+                h = mid-1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
