e0d5cad9fc0470ed12cece3d3e5b73609e45653f
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-03-25 21:57:06 -0600

    Time: 27 ms (85.03%), Space: 51.6 MB (49.89%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,9 +2,11 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        
-        HashSet<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         for(int k=0; k<n-2; k++){
+            if(k > 0 && nums[k] == nums[k-1]){
+                continue;
+            }
             int target = -nums[k];
             int i = k + 1, j = n - 1;
             while(i < j){
@@ -15,13 +17,16 @@ class Solution {
                     i++;
                 }
                 else{
-                    set.add(List.of(nums[k], nums[i], nums[j]));
+                    ans.add(List.of(nums[k], nums[i], nums[j]));
                     i++;
-                    j--;
+                    
+                    while(i < j && nums[i] == nums[i-1]){
+                        i++;
+                    }
                 }
             }
         }
 
-        return set.stream().collect(Collectors.toList());
+        return ans;
     }
 }
