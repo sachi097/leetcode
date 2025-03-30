ac6c113457d6ae364bf1cfda259a818f54de463c
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-03-29 18:16:27 -0600

    Time: 0 ms (100%), Space: 10.3 MB (34.79%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+         int N = matrix.size();
+        
+        //find transpose
+        for(int i = 0; i<N; i++) {
+            for(int j = i; j<N; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        //flip horizontally - For clock wise rotation
+        for(int i = 0; i<N; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
