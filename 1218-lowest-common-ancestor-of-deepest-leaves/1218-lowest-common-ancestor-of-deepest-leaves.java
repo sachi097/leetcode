f3f04a558148a50936400503f125660c176ea93e
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-04-03 22:05:21 -0600

    Time: 0 ms (100%), Space: 44.2 MB (66.84%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
@@ -13,53 +13,82 @@
  *     }
  * }
  */
+
+class Node {
+    TreeNode node;
+    int depth;
+
+    Node(TreeNode node, int depth){
+        this.node = node;
+        this.depth = depth;
+    }
+}
+
 class Solution {
-    TreeNode lca(TreeNode root, int a, int b){
+    // TreeNode lca(TreeNode root, int a, int b){
+    //     if(root == null){
+    //         return null;
+    //     }
+
+    //     if(root.val == a || root.val == b){
+    //         return root;
+    //     }
+
+    //     TreeNode leftLCA = lca(root.left, a, b);
+    //     TreeNode rightLCA = lca(root.right, a, b);
+
+    //     if(leftLCA != null && rightLCA != null){
+    //         return root;
+    //     }
+
+    //     return leftLCA != null ? leftLCA : rightLCA;
+    // }
+
+    public Node getLCA(TreeNode root){
         if(root == null){
-            return null;
+            return new Node(null, 0);
         }
 
-        if(root.val == a || root.val == b){
-            return root;
+        Node leftSubTree = getLCA(root.left);
+        Node rightSubTree = getLCA(root.right);
+
+        if(leftSubTree.depth == rightSubTree.depth){
+            return new Node(root, 1 + leftSubTree.depth);
         }
 
-        TreeNode leftLCA = lca(root.left, a, b);
-        TreeNode rightLCA = lca(root.right, a, b);
+        return new Node(leftSubTree.depth > rightSubTree.depth ? leftSubTree.node : rightSubTree.node, 1 + Math.max(leftSubTree.depth, rightSubTree.depth));
 
-        if(leftLCA != null && rightLCA != null){
-            return root;
-        }
-
-        return leftLCA != null ? leftLCA : rightLCA;
     }
 
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        ArrayDeque<TreeNode> q = new ArrayDeque<>();
-        int a=-1, b=-1;
-        q.add(root);
+        // ArrayDeque<TreeNode> q = new ArrayDeque<>();
+        // int a=-1, b=-1;
+        // q.add(root);
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode currNode = q.poll();
-                if(i==0){
-                    a = currNode.val;
-                }
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     for(int i=0; i<size; i++){
+        //         TreeNode currNode = q.poll();
+        //         if(i==0){
+        //             a = currNode.val;
+        //         }
 
-                if(i==size-1){
-                    b = currNode.val;
-                }
+        //         if(i==size-1){
+        //             b = currNode.val;
+        //         }
 
-                if(currNode.left != null){
-                    q.add(currNode.left);
-                }
+        //         if(currNode.left != null){
+        //             q.add(currNode.left);
+        //         }
 
-                if(currNode.right != null){
-                    q.add(currNode.right);
-                }
-            }
-        }
+        //         if(currNode.right != null){
+        //             q.add(currNode.right);
+        //         }
+        //     }
+        // }
 
-        return lca(root, a, b);
+        // return lca(root, a, b);
+
+        return getLCA(root).node;
     }
 }
