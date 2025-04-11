572e92f22754d02d0531712fcbc08665b75ccd41
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-04-10 22:50:08 -0600

    Time: 0 ms (100%), Space: 8.1 MB (98.69%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        priority_queue<pair<int, int>> pq;
-        unordered_map<int, int> hm;
-        for(auto ch: s){
-            hm[ch]++;
+        vector<int> charCounts(26, 0);
+        for (char c : s) {
+            charCounts[c - 'a']++;
         }
-        for(auto ele: hm){
-            pq.push({ele.second, ele.first});
+        int maxCount = 0, letter = 0;
+        for (int i = 0; i < charCounts.size(); i++) {
+            if (charCounts[i] > maxCount) {
+                maxCount = charCounts[i];
+                letter = i;
+            }
+        }
+        if (maxCount > (s.length() + 1) / 2) {
+            return "";
+        }
+        string ans = s;
+        int index = 0;
+
+        // Place the most frequent letter
+        while (charCounts[letter] != 0) {
+            ans[index] = char(letter + 'a');
+            index += 2;
+            charCounts[letter]--;
         }
 
-        string result = "";
-
-        while(!pq.empty()){
-            auto currTop = pq.top();
-            pq.pop();
-            if(result.size() > 0 && result[result.size() - 1] == currTop.second){
-                if(pq.empty()){
-                    return "";
+        // Place rest of the letters in any order
+        for (int i = 0; i < charCounts.size(); i++) {
+            while (charCounts[i] > 0) {
+                if (index >= s.length()) {
+                    index = 1;
                 }
-                auto nextTop = pq.top();
-                pq.pop();
-                nextTop.first--;
-                result += nextTop.second;
-                if(nextTop.first){
-                    pq.push(nextTop);
-                }
-            }
-            else{
-                currTop.first--;
-                result += currTop.second;
-            }
-
-            if(currTop.first){
-                pq.push(currTop);
+                ans[index] = char(i + 'a');
+                index += 2;
+                charCounts[i]--;
             }
         }
 
-        return result;
+        return ans;
     }
 };
