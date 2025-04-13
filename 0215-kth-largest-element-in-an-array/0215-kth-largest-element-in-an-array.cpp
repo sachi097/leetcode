5f3cea63d225eea6cb052d0b034730e614d6c442
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-04-12 19:59:00 -0600

    Time: 36 ms (42.34%), Space: 59.1 MB (98.92%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
-    int findPartition(vector<int>&nums, int start, int end) {
-        int i = start - 1;
-        int pivot = nums[end];
-        for(int j = start; j < end; ++j) {
-            if(nums[j] <= pivot) {
-                swap(nums[++i], nums[j]);
+    int quickPartition(vector<int>& nums, int L, int R) {
+        
+        int P = nums[L];
+        int i = L+1; //0
+        int j = R; //0
+        
+        while(i <= j) {
+            
+            if(nums[i] < P && nums[j] > P) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
             }
+            
+            if(nums[i] >= P) {
+                i++;
+            }
+            
+            if(nums[j] <= P) {
+                j--;
+            }
+            
         }
-        i = i+1;
-        swap(nums[i], nums[end]);
-        return i;
+        
+        swap(nums[L], nums[j]);
+        return j; //P is at jth index
     }
-    int quickSelect(vector<int>& nums, int n_k, int start, int end) {
-        int pindex = findPartition(nums, start, end);
-        if (pindex == n_k) return pindex;
-        if (pindex < n_k) {
-            // skip identical value : Last testcase leading to TLE
-            pindex += 1;
-            while(pindex < n_k && nums[pindex] == nums[pindex-1]) pindex++; 
 
-            return randPartition(nums, n_k, pindex, end);
-        } else {
-            // skip identical value : Last testcase leading to TLE
-            pindex -= 1;
-            while(pindex > n_k && nums[pindex] == nums[pindex+1]) pindex--; 
-
-            return randPartition(nums, n_k, start, pindex);
+    int quickSelect(vector<int> &nums, int left, int right, int k){
+        int p = quickPartition(nums, left, right);
+        if(p == k){
+            return nums[p];
         }
+        else if(p < k){
+            return quickSelect(nums, p+1, right, k);
+        }
+        return quickSelect(nums, left, p-1, k);
     }
-    int randPartition(vector<int>& nums, int n_k, int start, int end) {
-        int randIndex = start + rand() % (end-start+1);
-        swap(nums[end], nums[randIndex]);
-        return quickSelect(nums, n_k, start, end);
-    }
+
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        int pindex = randPartition(nums, n-k, 0, n-1);
-        return nums[pindex];
-
-                // using priority queue
+        // quick select
+        return quickSelect(nums, 0, nums.size()-1, k-1);
+        
+        // using priority queue
         // priority_queue<int, vector<int>, greater<int>> pq;
         // for(int i=0; i<k; i++){
         //     pq.push(nums[i]);
@@ -51,6 +55,5 @@ public:
         //     }
         // }
         // return pq.top();
-
     }
 };
