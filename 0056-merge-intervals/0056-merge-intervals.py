90f2281e0a8700d8d1761d737f06937b833924e1
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-11-12 17:26:55 -0800

    Time: 11 ms (30.05%), Space: 21.8 MB (16.81%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -9,7 +9,6 @@ class Solution:
             else:
                 start = end = 0
                 if result[-1][1] >= intervals[i][0]:
-                    result[-1][0] = min(result[-1][0], intervals[i][0])
                     result[-1][1] = max(result[-1][1], intervals[i][1])
                 else:
                     result.append([intervals[i][0], intervals[i][1]])
