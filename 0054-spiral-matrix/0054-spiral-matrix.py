10396b91d3a29fe5b27bf11c1cbabd7f09d8afc2
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2025-11-16 16:54:00 -0800

    Time: 0 ms (100%), Space: 18 MB (18.06%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,37 +1,32 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        start_row = 0
-        end_row = len(matrix) - 1
-        start_col = 0
-        end_col = len(matrix[0]) - 1
-
+        start_row, end_row, start_col, end_col = 0, len(matrix) - 1, 0, len(matrix[0]) - 1
         result = []
-        
+
         while start_row <= end_row and start_col <= end_col:
-            # print first row
-            for j in range(start_col, end_col+1):
+
+            # first row
+            for j in range(start_col, end_col + 1):
                 result.append(matrix[start_row][j])
             
             start_row += 1
 
-            # print last col
-            for i in range(start_row, end_row+1):
+            # last col
+            for i in range(start_row, end_row + 1):
                 result.append(matrix[i][end_col])
             
             end_col -= 1
 
-            #print last row
             if start_row <= end_row:
-                for j in range(end_col, start_col-1, -1):
+                for j in range(end_col, start_col - 1, -1):
                     result.append(matrix[end_row][j])
-                
-                end_row -= 1
             
-            #print first col
+                end_row -= 1
+
             if start_col <= end_col:
-                for i in range(end_row, start_row-1, -1):
+                for i in range(end_row, start_row - 1, -1):
                     result.append(matrix[i][start_col])
-                
+            
                 start_col += 1
         
         return result
